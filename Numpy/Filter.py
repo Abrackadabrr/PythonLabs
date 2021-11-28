0c63f54a9c15aaaa65b3d160a6204d85e32d9247
Abrackadabrr <iliynea2002@ya.ru>
2021-11-28 18:34:52 +0300

    Closing Filter issue

--- Numpy/Filter.py
+++ Numpy/Filter.py
@@ -12,9 +12,9 @@ for i in range(1, 4):
     with open(f'Ex2/signals/signal0{i}.dat') as file:
         data = np.array(file.readlines(), dtype=np.float64)
     # скользящее среднее - частный случай свёртки
-    res = list(np.convolve(data, np.ones(10)/10, mode='valid'))
+    res = np.convolve(data, np.ones(10)/10, mode='valid')
     res0 = [*map(slide_average, [0, 1, 2, 3, 4, 5, 6, 7, 8])]
-    res = np.array(res0 + res)
+    res = np.concatenate((res0, res))
 
     fix, (ax, ox) = plt.subplots(ncols=2)
     ox.plot(np.arange(0, data.size, 1), res)
